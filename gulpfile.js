8a0428ea5275bec44fef70c8c5f8928fe19554fd
醉墨 <zuimo@51xianqu.net>
2016-07-13 00:54:52 +0800

    利用express搭建简单博客系统

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify'),
+    minifyCSS =  require('gulp-minify-css');
+
+var DIR = {
+    less: 'public/stylesheets/*.less',
+    js: 'public/javascripts/*.js'
+};
+
+
+gulp.task('less-build', function() {
+    gulp.src(DIR.less)
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('public/stylesheets/'));
+});
+
+gulp.task('js-build', function() {
+    gulp.src(DIR.js)
+        .pipe(uglify())
+        .pipe(gulp.dest('public/javascripts/'));
+});
+
+gulp.task('default',[
+    'less-build',
+    'js-build'
+]);
