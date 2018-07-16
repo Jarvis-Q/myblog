8d5685a94afd266e818c0f894e22154d58b8a0e1
jarvis.shown <bird.jarvis@gmail.com>
2018-07-16 15:56:22 +0800

    update version

---
+++ src/common/config/db.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+exports.default = {
+    "type": "mysql",
+    "adapter": {
+        "mysql": {
+            "host": "127.0.0.1",
+            "port": "3306",
+            "database": "blog",
+            "user": "root",
+            "password": "zuimo4Sql!",
+            "prefix": "fk_",
+            "encoding": "utf8mb4"
+        }
+    }
+}
+    
